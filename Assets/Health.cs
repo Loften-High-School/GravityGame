833241f410810dc55cabfd3bb2eefa955a7cc2b2
DiceRoll <dangerpewpewpew@gmail.com>
2025-02-19 09:26:32 -0500

    Update Health.cs

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class HealthSystem : MonoBehaviour
 {
-  [SerializeField] private float StartingHealth;
-private float health;
+private float health = 3f;
+
+public void OnCollisionEnter (Collision collision)
+    {
+        if (collision.gameObject.CompareTag("laser"))
+        {
+          if (health = 3)
+          {
+            health = 2;
+          }
+          if (health = 2)
+           {
+           health = 1;
+           }
+           if (health = 1)
+           {
+            health = 0;
+           }
+        }
+    }
 public float Health
 {
   get
